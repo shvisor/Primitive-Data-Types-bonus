3dadcc05e8fa698309455c66cee4673b63a7286d
Max Shvagerus <mshvagerus@gmail.com>
2023-06-28 01:46:31 +0300

    add java doc

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,24 @@
 public class Main {
+    /**
+     * Сотовый оператор дарит клиенту по 1 рублю за каждые полные 100 рублей пополнения.
+     * @value incomingAmount - сумма на счете до пополнения.
+     * @value depositAmount - сумма пополнения.
+     * @value bonusAmount - расчет начисления бонуса.
+     */
     public static void main(String[] args) {
 
-        int incomingAmount = 500;                                    // сумма на счете до пополнения
-        int depositAmount = 1500;                                    // сумма пополнения
+        int incomingAmount = 500;
+        int depositAmount = 1500;
         boolean bonusCalc = depositAmount > 1000;
 
-        int bonusAmount;                                             // расчет начисления бонуса
+        int bonusAmount;
         if (bonusCalc) {
             bonusAmount = depositAmount / 100;
         } else {
             bonusAmount = 0;
         }
 
-        /* int bonusAmount = bonusCalc ? depositAmount / 100 : 0; */ // попробовал этот вариант, также все работает (похоже на функцию ЕСЛИ из Excel)
+//        int bonusAmount = bonusCalc ? depositAmount / 100 : 0; // попробовал этот вариант, также все работает (похоже на функцию ЕСЛИ из Excel)
 
         int account = incomingAmount + depositAmount + bonusAmount;
 
